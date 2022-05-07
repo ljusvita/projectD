a60f4cce8ee03527dff3a2daec9ae1cab53e9ee6
santos <santosfromseoul@gmail.com>
2022-05-07 23:31:09 +0900

    delete unused class & refactoring

--- lottery/src/test/groovy/com/example/lottery/LotteryControllerTest.groovy
+++ lottery/src/test/groovy/com/example/lottery/LotteryControllerTest.groovy
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
-import spock.lang.Ignore
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
