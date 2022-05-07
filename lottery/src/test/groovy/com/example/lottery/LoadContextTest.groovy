82593aa836454d91026ae90b0f2893949aa810af
santos <santosfromseoul@gmail.com>
2022-05-07 22:51:05 +0900

    [add] add springboot start web & load context test

---
+++ lottery/src/test/groovy/com/example/lottery/LoadContextTest.groovy
@@ -0,0 +1,17 @@
+package com.example.lottery
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class LoadContextTest extends Specification {
+
+    @Autowired (required = false)
+    private LotteryController lotteryController
+
+    def "when context is loaded then all expected beans are created"() {
+        expect: "the WebController is created"
+        lotteryController
+    }
+}
