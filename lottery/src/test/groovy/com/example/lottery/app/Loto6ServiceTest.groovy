5e629747dffcb8fb1fefa17f232883b3c85f9a9f
santos <santosfromseoul@gmail.com>
2022-05-28 23:56:36 +0900

    test refactoring

--- lottery/src/test/groovy/com/example/lottery/app/Loto6ServiceTest.groovy
+++ lottery/src/test/groovy/com/example/lottery/app/Loto6ServiceTest.groovy
@@ -1,6 +1,5 @@
 package com.example.lottery.app
 
-import org.junit.jupiter.api.Order
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -13,7 +12,6 @@ class Loto6ServiceTest extends Specification {
     @Autowired
     private WebTestClient webTestClient
 
-    @Ignore
     def "1부터 43까지의 랜덤한 6자리 자연수가 중복없이 선택되어 반환될것"() {
         given:
         Loto6Service target = new Loto6Service()
@@ -25,17 +23,5 @@ class Loto6ServiceTest extends Specification {
         then:
         actual.stream().filter( o -> expect.contains(o))
         actual.size() == expect.size()
-    }
-
-    @Order(1)
-    def "when get is performed then the response has status 200 and the content consist of json elements"() {
-        expect: "Status is 200 and the response consist of json elements"
-        webTestClient.get()
-                .uri("/loto6")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath('$.numbers6').isNotEmpty()
-//                .jsonPath('$.numbers6[0]').isEqualTo(1)
     }
 }
