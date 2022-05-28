c10d6a30ee92b0c68c912d4e8bcd6ef537ae6526
santos <santosfromseoul@gmail.com>
2022-05-29 00:32:36 +0900

    test refactoring

--- lottery/src/test/groovy/com/example/lottery/app/Loto6ServiceTest.groovy
+++ lottery/src/test/groovy/com/example/lottery/app/Loto6ServiceTest.groovy
@@ -1,25 +1,8 @@
 package com.example.lottery.app
 
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import spock.lang.Ignore
 import spock.lang.Specification
 
-@WebFluxTest(Loto6Controller.class)
 class Loto6ServiceTest extends Specification {
-
-    @Ignore
-    def "1부터 43까지의 랜덤한 6자리 자연수가 중복없이 선택되어 반환될것"() {
-        given:
-        Loto6Service target = new Loto6Service()
-        List<String> expect = List.of("1", "2", "3", "4", "5", "6")
-
-        when:
-        List<String> actual = target.pick()
-
-        then:
-        actual.stream().filter(o -> expect.contains(o))
-        actual.size() == expect.size()
-    }
 
     def "1부터 43까지의 랜덤한 6자리 자연수가 중복없이 선택되어 반환될것2"() {
         given:
