f9778baf4985b4e8b46bfcca36d61260203f3c22
santos <santosfromseoul@gmail.com>
2022-07-01 09:52:45 +0900

    add RouterFunction sample

--- lottery/src/main/java/com/example/lottery/LotteryApplication.java
+++ lottery/src/main/java/com/example/lottery/LotteryApplication.java
@@ -1,12 +1,25 @@
 package com.example.lottery;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class LotteryApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LotteryApplication.class, args);
+  }
+
+  @Bean
+  RouterFunction<ServerResponse> routes() {
+    return route(GET("/"), req -> ok().body(Flux.just("Hello", "World!"), String.class));
   }
 }
