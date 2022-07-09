e6f84a9fd2a98d7ed0742425394b1d6ad2cd0932
santos <santosfromseoul@gmail.com>
2022-07-09 23:16:53 +0900

    Revert "make loto7 routerfunction"
    This reverts commit 1422e8552f85cf076f1d2de539da52cd6ab04e59.

--- lottery/src/main/java/com/example/lottery/app/RouterConfig.java
+++ lottery/src/main/java/com/example/lottery/app/RouterConfig.java
@@ -12,23 +12,10 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 public class RouterConfig {
 
   @Bean
-  public RouterFunction<ServerResponse> getUsers(
-      MiniLotoHandler MiniLotoHandler, Loto7Handler loto7Handler) {
+  public RouterFunction<ServerResponse> getUsers(MiniLotoHandler MiniLotoHandler) {
     return RouterFunctions.route(
-            RequestPredicates.GET("/loto/mini")
-                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-            MiniLotoHandler::miniLoto)
-        .andRoute(
-            RequestPredicates.GET("/loto7")
-                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-            loto7Handler::loto7);
+        RequestPredicates.GET("/loto/mini")
+            .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
+        MiniLotoHandler::miniLoto);
   }
-
-  // @Bean
-  // public RouterFunction<ServerResponse> loto7(Loto7Handler loto7Handler) {
-  //   return RouterFunction.route(
-  //       RequestPredicates.GET("/loto7")
-  //         .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)),
-  //       loto7Handler::loto7);
-  // }
 }
