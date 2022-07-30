2ce0479ec8e9d579abe47eb5aa4a136f3dfb9342
santos <santosfromseoul@gmail.com>
2022-07-30 15:46:32 +0900

    add javadoc

--- lottery/src/main/java/com/example/lottery/app/MiniLotoHandler.java
+++ lottery/src/main/java/com/example/lottery/app/MiniLotoHandler.java
@@ -7,6 +7,9 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+/**
+ * 미니로또 핸들러
+ */
 @Component
 public class MiniLotoHandler {
 
