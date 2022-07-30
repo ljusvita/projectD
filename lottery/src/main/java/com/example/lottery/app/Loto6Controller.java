0ad4673d3df6308f80080604b9f0590097982b7c
santos <santosfromseoul@gmail.com>
2022-07-30 12:51:20 +0900

    Revert "test"
    This reverts commit a55658c843cb8644f8b21fa046f18db5c9ca6f88.

--- lottery/src/main/java/com/example/lottery/app/Loto6Controller.java
+++ lottery/src/main/java/com/example/lottery/app/Loto6Controller.java
@@ -1,9 +1,7 @@
 package com.example.lottery.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
