a55658c843cb8644f8b21fa046f18db5c9ca6f88
santos <santosfromseoul@gmail.com>
2022-07-30 12:46:34 +0900

    test

--- lottery/src/main/java/com/example/lottery/app/Loto6Controller.java
+++ lottery/src/main/java/com/example/lottery/app/Loto6Controller.java
@@ -1,7 +1,9 @@
 package com.example.lottery.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
