965ab898c0fac1c8fc24c78ef67629c6784b8724
santos <santosfromseoul@gmail.com>
2022-08-12 10:09:45 +0900

    add javadoc

--- lottery/src/main/java/com/example/lottery/app/MiniLotoHandler.java
+++ lottery/src/main/java/com/example/lottery/app/MiniLotoHandler.java
@@ -7,10 +7,16 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-/** 미니로또 핸들러 */
+/** 미니로또 핸들러. */
 @Component
 public class MiniLotoHandler {
 
+  /**
+   * 미니로또 번호를 반환하는 메소드.
+   *
+   * @param request http 리퀘스트 정보.
+   * @return 미니로또 5개 숫자를 http 스테이터스 200과 함께 반환.
+   */
   public Mono<ServerResponse> miniLoto(ServerRequest request) {
     MiniLoto miniLoto = new MiniLoto();
     miniLoto.setNumbers(List.of(1, 2, 3, 4, 5));
