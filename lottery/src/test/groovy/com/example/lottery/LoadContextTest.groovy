602e5c6618b2d65a9ab2302f5617d5f9373e0598
santos <santosfromseoul@gmail.com>
2022-08-20 17:21:21 +0900

    delete rest loto7 endpoint

--- lottery/src/test/groovy/com/example/lottery/LoadContextTest.groovy
+++ lottery/src/test/groovy/com/example/lottery/LoadContextTest.groovy
@@ -1,6 +1,6 @@
 package com.example.lottery
 
-import com.example.lottery.app.Loto7Controller
+import com.example.lottery.app.Loto6Controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Specification
@@ -8,11 +8,11 @@ import spock.lang.Specification
 @SpringBootTest
 class LoadContextTest extends Specification {
 
-    @Autowired (required = false)
-    private Loto7Controller loto7Controller
+    @Autowired(required = false)
+    private Loto6Controller loto6Controller
 
     def "when context is loaded then all expected beans are created"() {
         expect: "the WebController is created"
-        loto7Controller
+        loto6Controller
     }
 }
