3afbbb057e06e3437aee71f673cfd8e1e5ec28c5
santos <santosfromseoul@gmail.com>
2022-08-22 21:17:41 +0900

    apply spotless

--- lottery/src/main/java/com/example/lottery/app/Loto7Service.java
+++ lottery/src/main/java/com/example/lottery/app/Loto7Service.java
@@ -1,12 +1,9 @@
 package com.example.lottery.app;
 
-import java.util.Set;
-
 public class Loto7Service {
   public Loto7 pick() {
     Loto7 loto7 = new Loto7();
-    loto7.setNumbers(Set.of(1, 2, 3, 4, 5, 6, 7));
-
+    loto7.quickPick();
     return loto7;
   }
 }
