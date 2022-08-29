d3abe9b9597ebec53c48567ab92ecd0372ae816a
santos <santosfromseoul@gmail.com>
2022-08-29 21:45:14 +0900

    dockerize

--- ui_lottery/src/components/Loto7.vue
+++ ui_lottery/src/components/Loto7.vue
@@ -12,11 +12,13 @@
     Still loading..
   </p>
   <p v-if="error">
+    error! error!
   </p>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import {ref, onMounted} from "vue";
+
 export default {
   name: 'Loto7Post',
   props: {
@@ -31,42 +33,42 @@ export default {
       loading.value = true;
       // I prefer to use fetch
       // you can use use axios as an alternative
-      return fetch('http://127.0.0.1:18080/loto/7', {
+      return fetch('http://docker:18080/loto/7', {
         method: 'get',
         headers: {
           'content-type': 'application/json'
         }
       })
-        .then(res => {
-          // a non-200 response code
-          if (!res.ok) {
-            // create error instance with HTTP status text
-            const error = new Error(res.statusText);
-            error.json = res.json();
-            throw error;
-          }
-          console.log(res);
-          console.log(res.json());
+          .then(res => {
+            // a non-200 response code
+            if (!res.ok) {
+              // create error instance with HTTP status text
+              const error = new Error(res.statusText);
+              error.json = res.json();
+              throw error;
+            }
+            console.log(res);
+            console.log(res.json());
 
-          return res.json();
-        })
-        .then(json => {
-          // set the response data
-          data.value = json.data;
-        })
-        .catch(err => {
-          error.value = err;
-          // In case a custom JSON error response was provided
-          if (err.json) {
-            return err.json.then(json => {
-              // set the JSON response message
-              error.value.message = json.message;
-            });
-          }
-        })
-        .then(() => {
-          loading.value = false;
-        });
+            return res.json();
+          })
+          .then(json => {
+            // set the response data
+            data.value = json.data;
+          })
+          .catch(err => {
+            error.value = err;
+            // In case a custom JSON error response was provided
+            if (err.json) {
+              return err.json.then(json => {
+                // set the JSON response message
+                error.value.message = json.message;
+              });
+            }
+          })
+          .then(() => {
+            loading.value = false;
+          });
     }
 
     onMounted(() => {
