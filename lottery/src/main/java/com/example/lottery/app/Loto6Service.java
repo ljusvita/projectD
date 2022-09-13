4ce02d01bcedd88e8deb8e37bbb1f0062197a3ac
santos <santosfromseoul@gmail.com>
2022-09-13 16:02:14 +0900

    mod tests

--- lottery/src/main/java/com/example/lottery/app/Loto6Service.java
+++ lottery/src/main/java/com/example/lottery/app/Loto6Service.java
@@ -6,7 +6,8 @@ public class Loto6Service {
 
   public Loto6 pick() {
     Loto6 loto6 = new Loto6();
-    loto6.setNumbers(List.of(1, 2, 3, 4, 5, 6));
+//    loto6.setNumbers(List.of(1, 2, 3, 4, 5, 6));
+    loto6.quickPick();
     return loto6;
   }
 }
--- lottery/src/test/groovy/com/example/lottery/app/Loto6Test.groovy
+++ lottery/src/test/groovy/com/example/lottery/app/Loto6Test.groovy
@@ -20,4 +20,25 @@ class Loto6Test extends Specification {
         then:
         loto6.getNumbers().size() == 6
     }
+
+    def "로또6 퀵픽 실행 후 매번 다른 결과가 생성될 것"() {
+        // 테스트 대상의 실행에 전제되는 조건
+        given:
+        Loto6 firstGameOfLoto6 = new Loto6()
+        firstGameOfLoto6.quickPick()
+
+        Loto6 secondGameOfLoto6 = new Loto6()
+        secondGameOfLoto6.quickPick()
+
+        // 테스트 대상의 실행
+        when:
+        var firstGame = firstGameOfLoto6.getNumbers()
+        var secondGame = secondGameOfLoto6.getNumbers()
+
+        // 테스트 대상의 실행 결과
+        then:
+        firstGame.size() == 6
+        secondGame.size() == 6
+        firstGame != secondGame
+    }
 }
