8bc33abc3282a3a0e4832bf0dc4497449421436d
santos <santosfromseoul@gmail.com>
2022-09-16 15:53:59 +0900

    change loto6 with builder pattern

--- lottery/src/main/java/com/example/lottery/app/Loto6.java
+++ lottery/src/main/java/com/example/lottery/app/Loto6.java
@@ -11,33 +11,18 @@ public class Loto6 {
 
   private static final int LOTO6_NECESSARY_NUMBER = 6;
 
-  public Loto6() {
-    this.numbers = new HashSet<>();
-  }
-
   public Loto6(builder builder) {
     this.numbers = builder.pickingNumbers;
-  }
-
-  public void setNumbers(Set<Integer> numbers6) {
-    this.numbers = numbers6;
   }
 
   public Set<Integer> getNumbers() {
     return numbers;
   }
 
-  public void quickPick() {
-    while (this.numbers.size() < 6) {
-      this.numbers.add(RandomUtils.nextInt(1, 43));
-    }
-  }
-
   public static class builder {
-
     private Set<Integer> pickingNumbers = new HashSet<>();
 
-    /** 자동선택 메소드. 선택된 번호가 7개가 될때까지 1부터 37까지의 숫자를 골라 저장 */
+    /** 자동선택 메소드. 선택된 번호가 6개가 될때까지 1부터 43까지의 숫자를 골라 저장 */
     public Loto6.builder quickPick() {
       while (pickingNumbers.size() < LOTO6_NECESSARY_NUMBER) {
         pickingNumbers.add(RandomUtils.nextInt(1, 43));
