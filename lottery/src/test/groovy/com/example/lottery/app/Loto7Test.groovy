ed05118586ff755e2d2505bc108daf9231dbcf28
santos <santosfromseoul@gmail.com>
2022-09-16 15:33:55 +0900

    refactoring

--- lottery/src/test/groovy/com/example/lottery/app/Loto7Test.groovy
+++ lottery/src/test/groovy/com/example/lottery/app/Loto7Test.groovy
@@ -10,7 +10,7 @@ class Loto7Test extends Specification {
         assert loto7.getNumbers().size() == 0
     }
 
-    def "로또7이 랜덤으로 7개 무작위 번호를 생성 할 것2"() {
+    def "로또7이 랜덤으로 7개 무작위 번호를 생성 할 것"() {
         // 테스트 대상의 실행에 전제되는 조건 & 테스트 대상의 실행
         given:
         Loto7 loto7
