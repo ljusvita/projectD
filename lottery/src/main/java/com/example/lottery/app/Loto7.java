4db488e98e3549d55a852b4a554247f576114c51
santos <santosfromseoul@gmail.com>
2022-09-16 15:44:27 +0900

    refactoring

--- lottery/src/main/java/com/example/lottery/app/Loto7.java
+++ lottery/src/main/java/com/example/lottery/app/Loto7.java
@@ -14,22 +14,8 @@ public class Loto7 {
 
   private static final int LOTO7_NECESSARY_NUMBER = 7;
 
-  /** 인스턴스화 할 시 빈 Set 타입으로 초기화 */
-  public Loto7() {
-    this.numbers = new HashSet<>();
-  }
-
   public Loto7(builder builder) {
     this.numbers = builder.pickingNumbers;
-  }
-
-  /**
-   * setter. Set 타입 선택된 번호를 저장하는 메소드
-   *
-   * @param numbers 선택된 로또7 번호를 저장하는 Set
-   */
-  public void setNumbers(Set<Integer> numbers) {
-    this.numbers = numbers;
   }
 
   /**
@@ -41,16 +27,10 @@ public class Loto7 {
     return this.numbers.stream().sorted().collect(Collectors.toList());
   }
 
-  /** 자동선택 메소드. 선택된 번호가 7개가 될때까지 1부터 37까지의 숫자를 골라 저장 */
-  public void quickPick() {
-    while (this.numbers.size() < LOTO7_NECESSARY_NUMBER) {
-      this.numbers.add(RandomUtils.nextInt(1, 38));
-    }
-  }
-
   public static class builder {
     private Set<Integer> pickingNumbers = new HashSet<>();
 
+    /** 자동선택 메소드. 선택된 번호가 7개가 될때까지 1부터 37까지의 숫자를 골라 저장 */
     public builder quickPick() {
       while (pickingNumbers.size() < LOTO7_NECESSARY_NUMBER) {
         pickingNumbers.add(RandomUtils.nextInt(1, 38));
