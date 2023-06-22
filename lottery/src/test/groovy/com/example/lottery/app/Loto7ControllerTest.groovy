0f371239a33fe13172fc8dde06a8125581fcea91
santos <santosfromseoul@gmail.com>
2023-06-22 11:57:24 +0900

    add loto7 rest controller

---
+++ lottery/src/test/groovy/com/example/lottery/app/Loto7ControllerTest.groovy
@@ -0,0 +1,28 @@
+package com.example.lottery.app
+
+import org.junit.jupiter.api.Order
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import spock.lang.Specification
+
+@WebFluxTest(Loto7Controller.class)
+class Loto7ControllerTest extends Specification {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Order(1)
+    def "get 메소드가 실행되면 response 스테이터스 코드는 200이고 json 요소로 구성되어 있을 것"() {
+        expect: "스테이터스 코드 200 이고 numbers 라는 이름의 7개의 요소를 가진 json 객체일 것"
+        webTestClient.get()
+                .uri("/loto7")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath('$.numbers').isNotEmpty()
+                .jsonPath('$.numbers').isArray()
+                .consumeWith(
+                        result -> result.getResponseBody().size() == 7)
+    }
+}
