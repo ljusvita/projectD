cb072d2e19a18e2335b53f4ff7de3ebb76edaf8d
santos <santosfromseoul@gmail.com>
2023-06-22 16:27:47 +0900

    add cross origin annotation

--- lottery/src/main/java/com/example/lottery/app/Loto7Controller.java
+++ lottery/src/main/java/com/example/lottery/app/Loto7Controller.java
@@ -1,5 +1,6 @@
 package com.example.lottery.app;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/loto7")
+@CrossOrigin("*")
 public class Loto7Controller {
 
   @GetMapping
   public Mono<Loto7> loto7() {
+    System.out.println("executed");
     return Mono.just(new Loto7.builder().quickPick().build());
   }
 }
