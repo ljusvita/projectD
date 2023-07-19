f6f0deecd842e91d9c71d17a914004ad86ed3194
santos <tuneful-maestro0z@icloud.com>
2023-07-19 17:17:34 +0900

    add test

--- gatherer/test/test_gatherer.py
+++ gatherer/test/test_gatherer.py
@@ -1,5 +1,8 @@
 import pytest
-import gatherer.loto7.loto7_gatherer
+from loto7.loto7_gatherer import Loto7Gatherer
 
 def test_test():
     assert True == True
+
+def test_gatherer():
+    assert Loto7Gatherer != None
