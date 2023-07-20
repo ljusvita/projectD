3984b8dc3b5d16af71bfdf8eeee3a0fd217501c3
santos <tuneful-maestro0z@icloud.com>
2023-07-20 13:56:43 +0900

    add test integer casting

--- gatherer/test/test_gatherer.py
+++ gatherer/test/test_gatherer.py
@@ -1,8 +1,8 @@
 import pytest
 from ..loto7.loto7_gatherer import Loto7Gatherer
 
-def test_test():
-    assert True == True
+def test_transfer_integer():
+    assert 4 == int('04')
 
 def test_gatherer():
     g = Loto7Gatherer()
